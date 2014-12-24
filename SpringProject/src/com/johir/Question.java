b728ca49402e40ff402688f76dcc1154561477b2
johirbuet <johirbuet@Johir>
2014-12-23 22:06:30 -0600

    By Md Johirul Islam

--- SpringProject/src/com/johir/Question.java
+++ SpringProject/src/com/johir/Question.java
@@ -2,12 +2,16 @@ package com.johir;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class Question {
 	private int id;
 	private String name;
-	private List<Answer> answers;
-	public Question(int id,String name,List<Answer>answers) {
+	//private List<Answer> answers;
+	private Map<String,String> answers;
+	public Question(int id,String name,Map<String,String> answers) {
 		// TODO Auto-generated constructor stub
 		this.id=id;
 		this.name=name;
@@ -16,9 +20,11 @@ public class Question {
 	public void displayInfo(){
 		System.out.println(" "+id+"  "+name);
 		System.out.println("Answers are:");
-		Iterator<Answer> iter=answers.iterator();
+		Set<Entry<String, String>> set=answers.entrySet();
+		Iterator iter=set.iterator();
 		while(iter.hasNext()){
-			System.out.println(iter.next());
+			Entry<String,String> entry=(Entry<String, String>) iter.next();
+			System.out.println("Answer:"+entry.getKey()+" Posted By:"+entry.getValue());
 		}
 	}
 
